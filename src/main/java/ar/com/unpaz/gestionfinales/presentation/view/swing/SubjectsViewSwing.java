f88c880f26851ce3127bae91a0a6d4f48238c13f
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-10-07 15:37:39 -0300

    First implementation of material view in swing.

--- src/main/java/ar/com/unpaz/gestionfinales/presentation/view/swing/SubjectsViewSwing.java
+++ src/main/java/ar/com/unpaz/gestionfinales/presentation/view/swing/SubjectsViewSwing.java
@@ -1,26 +1,73 @@
 package ar.com.unpaz.gestionfinales.presentation.view.swing;
 
+import static ar.com.unpaz.gestionfinales.presentation.view.swing.IconResource.load;
+import static ar.com.unpaz.gestionfinales.presentation.view.swing.IconResource.IconPathOf.ADD;
+import static ar.com.unpaz.gestionfinales.presentation.view.swing.IconResource.IconPathOf.DELETE;
+import static ar.com.unpaz.gestionfinales.presentation.view.swing.IconResource.IconPathOf.UPDATE;
+import static java.awt.BorderLayout.CENTER;
+import static java.awt.BorderLayout.NORTH;
+import static java.awt.BorderLayout.SOUTH;
+import static java.awt.FlowLayout.LEFT;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import ar.com.unpaz.gestionfinales.domain.Year;
 import ar.com.unpaz.gestionfinales.presentation.view.SubjectView;
 
 public class SubjectsViewSwing implements SubjectView {
 
+  private JDialog dialog;
   private JTable tableSubjects;
   private JComboBox<Year> comboxYear;
   private JButton buttonAdd;
   private JButton buttonUpdate;
   private JButton buttonDelete;
-  
+
   public SubjectsViewSwing() {
-    
+    dialog = new JDialog();
+    dialog.setModal(true);
+    dialog.setSize(300, 300);
+    dialog.getContentPane().setLayout(new BorderLayout());
+    dialog.getContentPane().add(createNorthPane(), NORTH);
+    dialog.getContentPane().add(createCenterPane(), CENTER);
+    dialog.getContentPane().add(createSouthPane(), SOUTH);
+  }
+
+  private JPanel createNorthPane() {
+    JPanel pane = new JPanel(new FlowLayout(LEFT));
+    comboxYear = new JComboBox<>();
+    pane.add(comboxYear);
+    return pane;
+  }
+
+  private JScrollPane createCenterPane() {
+    JScrollPane pane = new JScrollPane();
+    pane.setVerticalScrollBarPolicy(22);
+    tableSubjects = new JTable();
+    pane.setViewportView(tableSubjects);
+    return pane;
+  }
+
+  private JPanel createSouthPane() {
+    JPanel pane = new JPanel();
+    buttonAdd = new JButton(load(ADD));
+    buttonUpdate = new JButton(load(UPDATE));
+    buttonDelete = new JButton(load(DELETE));
+    pane.add(buttonAdd);
+    pane.add(buttonUpdate);
+    pane.add(buttonDelete);
+    return pane;
   }
 
   @Override
   public void show() {
-   System.out.println("SubjectViewSwing.show");
+    dialog.setLocationRelativeTo(dialog.getParent());
+    dialog.setVisible(true);
   }
-  
+
 }
