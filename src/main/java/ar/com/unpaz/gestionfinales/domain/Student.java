09243c5b4e6bd42e88b63f77b5e1509316da91fe
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-10-09 17:00:01 -0300

    Adding Student entity

---
+++ src/main/java/ar/com/unpaz/gestionfinales/domain/Student.java
@@ -0,0 +1,28 @@
+package ar.com.unpaz.gestionfinales.domain;
+
+public class Student extends Entity {
+
+  private final String name;
+  private final String surname;
+  private final String email;
+
+  public Student(int dni, String name, String surname, String email) {
+    super(dni);
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+}
