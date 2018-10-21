df8f6bab7293ed4b6d99d9d737c52249cdbf6ab6
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-10-21 10:56:31 -0300

    Adding unit tests to the Subject class.

--- src/test/java/ar/com/unpaz/gestionfinales/domain/SubjectTest.java
+++ src/test/java/ar/com/unpaz/gestionfinales/domain/SubjectTest.java
@@ -16,6 +16,16 @@ public class SubjectTest {
     assertEquals(expectedYear, subject.getYear());
   }
 
+  @Test(expected = NullPointerException.class)
+  public void shouldNotCreateASubjectWithANullDescription() {
+    new Subject(null,Year.NONE);
+  }
+  
+  @Test(expected = NullPointerException.class)
+  public void shouldNotCreateASubjectWithANullYear() {
+    new Subject("",null);
+  }
+  
   @Test
   public void aSubjectShouldStoreADescriptionAndYear() {
     Subject subject = new Subject("A Description", Year.FIRST);
@@ -38,4 +48,25 @@ public class SubjectTest {
     assertEquals(expectedYear, subject.getYear());
   }
 
+  @Test
+  public void shouldIndicateIfItHasASpecificYear() {
+    Subject subject = new Subject("",Year.SECOND);
+    assertFalse(subject.haveThisYear(Year.FIRST));
+    assertTrue(subject.haveThisYear(Year.SECOND));
+  }
+  
+  @Test
+  public void shouldIndicateIfItsDescriptionIsEmpty() {
+    Subject subject = new Subject();
+    assertTrue(subject.hasEmptyDescription());
+    Subject subject2 = new Subject("-",Year.NONE);
+    assertFalse(subject2.hasEmptyDescription());
+  }
+  
+  @Test
+  public void theMaximumNumberOfCharactersInADescriptionOf50() {
+    int expectedMaximin = 50;
+    assertEquals(expectedMaximin , Subject.MAX_NUMBER_OF_CHARACTERS);
+  }
+  
 }
