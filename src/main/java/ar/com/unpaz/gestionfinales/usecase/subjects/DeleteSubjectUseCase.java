527ead2a4c4ede9557718db06f98c0abe109c7e7
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-10-22 17:19:01 -0300

    Refactoring the AddSubjectUseCase class.

--- src/main/java/ar/com/unpaz/gestionfinales/usecase/subjects/DeleteSubjectUseCase.java
+++ src/main/java/ar/com/unpaz/gestionfinales/usecase/subjects/DeleteSubjectUseCase.java
@@ -1,23 +1,41 @@
 package ar.com.unpaz.gestionfinales.usecase.subjects;
 
+import java.util.List;
 import ar.com.unpaz.gestionfinales.database.AppRepositoryContext;
 import ar.com.unpaz.gestionfinales.domain.Subject;
 import ar.com.unpaz.gestionfinales.presentation.AppViewContext;
-import ar.com.unpaz.gestionfinales.usecase.DialogController;
+import ar.com.unpaz.gestionfinales.validation.NoSubjectValidator;
+import ar.com.unpaz.gestionfinales.validation.Validator;
 
-public class DeleteSubjectUseCase implements DialogController {
+public class DeleteSubjectUseCase extends SkeletonSubjectUseCase {
+
+  public DeleteSubjectUseCase() {
+    this(new NoSubjectValidator());
+  }
+
+  public DeleteSubjectUseCase(Validator<Subject> validator) {
+    super(validator);
+  }
 
   @Override
-  public void accept() {
-    Subject subject = AppViewContext.delSubjectDialog.getSubject();
+  Subject getSubject() {
+    return AppViewContext.delSubjectDialog.getSubject();
+  }
+
+  @Override
+  void executeAction(Subject subject) {
     AppRepositoryContext.subjectRepository.remove(subject);
-    AppViewContext.subjectsView.setSubjects(AppRepositoryContext.subjectRepository.getAll());
+    List<Subject> all = AppRepositoryContext.subjectRepository.getAll();
+    AppViewContext.subjectsView.setSubjects(all);
     AppViewContext.delSubjectDialog.close();
   }
 
   @Override
-  public void cancel() {
+  void showError(String errorMessage) {}
+
+  @Override
+  void cancelAction() {
     AppViewContext.delSubjectDialog.close();
   }
-  
+
 }
